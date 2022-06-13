71d9dd534c11d4ddcafd43ffcc1acd32df40cf45
SirAlyon <alessiosada04@gmail.com>
2022-06-13 17:59:33 +0200

    add card

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,6 +11,7 @@ class PageController extends Controller
     public function index()
     {
         $movies = Movie::all();
+        
         return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,22 @@
 @section('content')
 
 <h1>I'm a test</h1>
+<div class="container">
+    <div class="row">
+        
+        @foreach($movies as $movie)
 
+        <div class="col-4">
+
+            <img class="img-fluid" src="https://picsum.photos/id/237/200" alt="">
+
+            <div class="title">{{$movie->title}}</div>
+            <div>{{$movie->original_title}}</div>
+        </div>
+
+        @endforeach
+
+    </div>
+</div>
 
 @endsection
